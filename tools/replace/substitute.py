bc0c431b0d21d5cb3166c6314adc2479158fc47e
gqw <994321849@qq.com>
2023-09-14 15:52:49 +0800

    add rotary float32

--- tools/replace/substitute.py
+++ tools/replace/substitute.py
@@ -1,8 +1,26 @@
+import torch
+import torch_dipu
 def apply_rotary(x1, x2, cos, sin, conj):
+    x1 = x1.to(torch.float32)
+    x2 = x2.to(torch.float32)
+    cos = cos.to(torch.float32)
+    sin = sin.to(torch.float32)
     if not conj:
         out1 = x1 * cos - x2 * sin
         out2 = x1 * sin + x2 * cos
     else:
         out1 = x1 * cos + x2 * sin
         out2 = -x1 * sin + x2 * cos
+    out1 = out1.to(torch.float16)
+    out2 = out2.to(torch.float16)
     return out1, out2
+
+# def apply_rotary(x1, x2, cos, sin, conj):
+#     if not conj:
+#         out1 = x1 * cos - x2 * sin
+#         out2 = x1 * sin + x2 * cos
+#     else:
+#         out1 = x1 * cos + x2 * sin
+#         out2 = -x1 * sin + x2 * cos
+#     return out1, out2
+
