c3332b1a254201b22b5bfed33159ae3d2a2e3088
POI-WX <wangxing2@sensetime.com>
2024-02-29 10:56:27 +0800

    update index used in profiling

--- train.py
+++ train.py
@@ -306,10 +306,10 @@ def main(args):
             if batch_count % 2 == 0:
                 prof.step()
                 index += 1
-                if index == 256 and args.profiling:
+                if index == 140 and args.profiling:
                    print("start e2e profiler", flush=True)
                    e2eprof.__enter__()
-                if index == 257 and args.profiling:
+                if index == 141 and args.profiling:
                     print("end e2e profiler", flush=True)
                     e2eprof.__exit__(None, None, None)
 
